dbb8474e99acd2dd47a5f3875b30fcf5870ed7c7
oppara <oppara@oppara.tv>
2016-10-18 02:02:57 +0900

    Fix logged in layout

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -49,13 +49,13 @@
                     <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
                         <!-- Authentication Links -->
-                        @if (Auth::guest())
+                        @if (Auth::guard('admin')->guest())
                             <li><a href="{{ url('/admin/login') }}">Login</a></li>
                             <li><a href="{{ url('/asmin/register') }}">Register</a></li>
                         @else
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                {{ Auth::guard('admin')->user()->name }} <span class="caret"></span>
                                 </a>
 
                                 <ul class="dropdown-menu" role="menu">
