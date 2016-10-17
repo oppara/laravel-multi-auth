c31ac711e816fb9d519cb910d58beff42da557c1
oppara <oppara@oppara.tv>
2016-10-18 01:36:23 +0900

    Fix Authenticate

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Auth;
 
 class Authenticate
 {
@@ -11,10 +12,22 @@ class Authenticate
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \Closure  $next
+     * @param  string|null  $guard
      * @return mixed
      */
-    public function handle($request, Closure $next)
+    public function handle($request, Closure $next, $guard = null)
     {
+        if (Auth::guard($guard)->guest()) {
+            if ($request->ajax() || $request->wantsJson()) {
+                return response('Unauthorized.', 401);
+            } else {
+                if ($guard == 'admin') {
+                    return redirect()->guest('/admin/login');
+                }
+                return redirect()->guest('login');
+            }
+        }
+
         return $next($request);
     }
 }
