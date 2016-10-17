64a64babf8c4c3ab03b853d11c13e81675455650
oppara <oppara@oppara.tv>
2016-10-18 01:52:17 +0900

    Fix AdminAuthController

--- app/Http/Controllers/AdminAuthController.php
+++ app/Http/Controllers/AdminAuthController.php
@@ -3,10 +3,39 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
 
-use App\Http\Requests;
 
 class AdminAuthController extends Controller
 {
-    //
+    use AuthenticatesUsers;
+
+    protected $guard = 'admin';
+    protected $redirectTo = '/admin/home';
+
+    protected $redirectAfterLogout = 'admin/login';
+
+    protected $username = 'email';
+
+    protected $maxLoginAttempts = 5;
+    protected $lockoutTime = 60;
+
+
+    public function showLoginForm()
+    {
+        return view('admin.login');
+    }
+
+    public function getGuard()
+    {
+        return $this->guard;
+    }
+
+    protected function guard()
+    {
+        return Auth::guard($this->getGuard());
+    }
+
+
 }
